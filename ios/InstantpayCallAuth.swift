b940ac21490619fc8e406ebb398a08183240480c
DJ <dhananjay.y@instantpay.in>
2024-02-16 15:04:59 +0530

    addd

--- ios/InstantpayCallAuth.swift
+++ ios/InstantpayCallAuth.swift
@@ -1,8 +1,40 @@
 @objc(InstantpayCallAuth)
-class InstantpayCallAuth: NSObject {
+class InstantpayCallAuth: RCTEventEmitter {
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
+    private var hasListeners = false;
+    
+    @objc(multiply:withB:withResolver:withRejecter:)
+    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve(a*b)
+    }
+
+    // we need to override this method and
+    // return an array of event names that we can listen to
+    override func supportedEvents() -> [String]! {
+        return ["CallAuthDidUpdateState"]
+    }
+    
+    // you also need to add the override attribute
+    // on these methods
+    override func constantsToExport() -> [AnyHashable: Any] {
+        return ["initialCount": 0];
+    }
+    
+    override static func requiresMainQueueSetup() -> Bool {
+        return false;
+    }
+    
+    override func startObserving(){
+        
+        hasListeners = true;
+        
+        //setup the listener
+    }
+    
+    override func stopObserving(){
+        
+        hasListeners = false;
+        
+        //stop the listener
+    }
 }
